7f45734afac1e537cc94d0e3a6e52fe8b5e14795
Sadirbek Muratov <sadirbekmuratov@gmail.com>
2022-12-12 00:28:25 +0600

    homework-2

--- example.py
+++ example.py
@@ -1,22 +1,45 @@
 class Human:
-    head =1
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+    head=1
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
     def __str__(self):
-        return f'name is {self.name}\n'\
-               f'age is {self.age}'
+        return f'name is {self.name}\n' \
+               f'age is {self.age}\n'
+
     def run(self):
-        print(f'{self.run} is running')
-hum = Human('Sadir', 36)
-print(hum)
+        print(f'{self.name} is running')
+
+hum=Human('Sadir',36)
+print(hum,hum.head)
 hum.run()
 
-
+# дочерний класс
 class Student(Human):
-    pass
-student=Student('Daniel', 60)
-print(student)
+    head = 2
+    def __init__(self, name, age,lastname):
+        super().__init__(name, age)
+        Human.__init__(self,name,age)
+        self.lastname=lastname
+    def fly(self):
+        print(self.name, 'is fly in True')
 
+    def __str__(self):
+        return f'name is {self.name}\n' \
+               f'age is {self.age}\n' \
+               f'lastname is {self.lastname}\n'
 
+student=Student('Daniyel',60,'Ermekov')
 
+print(student,student.head)
+# student.run()
+student.fly()
+
+# дочерний класс
+class Teacher(Student):
+    def fly(self):
+        print(f'{self.name} is flying')
+
+tech=Teacher('Beka',55,'DJU')
+tech.fly()
+# tech.run()
--- main.py
+++ main.py
@@ -18,11 +18,68 @@ class SuperHero:
 
 s = SuperHero('Superman', 'super', 'power','never dead', 'cathing')
 
-print(s.name)
-print()
-print(s.health_points *2)
-print(len(s.catchphrase))
-print(s)
+# print(s.name)
+# print()
+# print(s.health_points *2)
+# print(len(s.catchphrase))
+# print(s)
 
-h = SuperHero('Spiderman', 'spider', 'power','jumping', 'cathing')
-h.Hero()
+# h = SuperHero('Spiderman', 'spider', 'power','jumping', 'cathing')
+# h.Hero()
+
+
+class SuperHero_1(SuperHero):
+    h = 1
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, fly=False):
+        SuperHero.__init__(self,name, nickname, superpower, health_points,catchphrase)
+
+        self.fly = fly
+    def print_name_hero(self):
+        print(f'{self.health_points ** 2 }')
+        self.fly = True
+
+    def fly_true(self):
+        print('fly in the True_phrase')
+
+    # def fly(self):
+    #     print(self.name, 'is fly in False')
+
+s1 = SuperHero_1('hero1', 'heeroo','power', 'never', 'catch')
+print(s1)
+# s1.fly()
+
+class SuperHero_2(SuperHero):
+
+    k = 1
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, fly=False):
+        SuperHero.__init__(self,name, nickname, superpower, health_points,catchphrase)
+
+        self.fly = fly
+    def print_name_hero(self):
+        print(f'{self.health_points ** 2}')
+        self.fly = True
+
+    def fly_true(self):
+        print('fly in the True_phrase')
+
+class SuperHero_3(SuperHero):
+
+        z = 1
+        def __init__(self, name, nickname, superpower, health_points, catchphrase, fly=False):
+
+            SuperHero.__init__(self, name, nickname, superpower, health_points, catchphrase)
+
+            self.fly = fly
+
+
+        def print_name_hero(self):
+            print(f'{self.health_points ** 2}')
+            self.fly = True
+
+
+        def fly_true(self):
+            print('fly in the True_phrase')
+
+f=SuperHero_1('hero1', 'heeroo','power', 'never', 'catch')
+f.fly_true()
+f.print_name_hero()
